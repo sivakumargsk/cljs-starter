bb437841cbf41c16856bbcce075f6355ec54f37b
sivakumargsk <geddasiva777@gmail.com>
2016-03-17 17:30:00 +0530

    Initial Commit

---
+++ src/cljs/cljs_starter/app.cljs
@@ -0,0 +1,17 @@
+(ns cljs-starter.app
+  (:require [reagent.core :as reagent :refer [atom render]]
+            [sablono.core :as sab]
+            [devcards.core :as dc]
+            [cljs.test :as t :refer [report] :include-macros true]
+            [devtools.core :as devtools])
+  (:require-macros [devcards.core :as dc :refer [defcard deftest]]
+                   [cljs.test :refer [is testing async]]))
+
+(enable-console-print!)
+
+(defn init []
+  #_(render  [:h1 "Hello, Clojurescript"]
+             (.getElementById js/document "main-app-area"))
+  (devtools/set-pref! :install-sanity-hints true)
+  (devtools/install!)
+  (devcards.core/start-devcard-ui!))
