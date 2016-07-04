dfdcc08a72a639f338008e1e17321f89a1d8bf96
sivakumargsk <geddasiva777@gmail.com>
2016-07-04 01:19:17 +0000

    circular progress bar is created using canvas

--- src/cljs/cljs_starter/app.cljs
+++ src/cljs/cljs_starter/app.cljs
@@ -15,3 +15,41 @@
   (devtools/set-pref! :install-sanity-hints true)
   (devtools/install!)
   (devcards.core/start-devcard-ui!))
+
+
+(defn progresSim [all ctx cw ch]
+  (let [diff (.toFixed (* (/ @all 100) Math/PI 2 10) 2)]
+    (.clearRect ctx 0 0 cw ch)
+    (set! (.-lineWidth ctx) 25)
+    (set! (.-fillStyle ctx) "#09F")
+    (set! (.-strokeStyle ctx) "#09F")
+    (set! (.-textAlign ctx) "center")
+    (.fillText ctx (str @all "%") (* cw 0.5) (+ (* ch 0.5) 2) cw)
+    (.beginPath ctx)
+    (.arc ctx 70 70 45 4.72 (+ 4.72 (/ diff 10)) false)
+    (.stroke ctx)
+    (if (>= @all 100)
+      (js/clearTimeout (js/setInterval (fn [] (progresSim all ctx cw ch)) 50))
+      (swap! all inc))))
+
+(defn my-component []
+  (reagent/create-class
+   {:component-did-mount
+    #(let [all (reagent/atom 0)
+           ctx (.getContext
+                (.getElementById js/document "mycanvas")
+                "2d")
+           cw (.-width (.-canvas ctx))
+           ch (.-height (.-canvas ctx))]
+       (js/setInterval (fn [] (progresSim all ctx cw ch)) 50))
+    :reagent-render
+    (fn []
+      [:div [:canvas {:id "mycanvas"
+                      :width 150
+                      :height 150
+                      :style {:border "1px dashed #ccc"}}]])}))
+
+
+
+(defcard canvas-cicular-progress
+  (reagent/as-element [my-component]))
